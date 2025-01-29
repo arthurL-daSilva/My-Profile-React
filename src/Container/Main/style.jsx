469b4f8563a0af9176db057a82eb8ad32c385b68
arthurL-daSilva <arthurleal723@gmail.com>
2025-01-28 21:32:35 -0300

    Feito ajustes no Main, added animate.style framework

--- src/Container/Main/style.jsx
+++ src/Container/Main/style.jsx
@@ -1,20 +1,29 @@
 import styled from "styled-components"
+import 'animate.css'
 
 export const Container = styled.div`
     display: flex;
-    flex-direction: row;
     justify-content: space-evenly;
-    height: 849px;
+    align-items: center;
+    margin-top: 100px;
+    /* height: 849px; */
+    min-width: 700px;
+    height: 600px;
     font-family: "Jura",serif;
     color: #fff;
-    img{
-        
+    border-radius: 24px;
+    background-color: rgba(255, 255, 255, 0.2);
+    backdrop-filter: blur(5px);
+
+    div{
+        overflow: hidden;
     }
 
     aside{
         display: flex;
         flex-direction: column;
         justify-content: center;
+        animation-duration: 2s;
     }
 
     aside > p{
@@ -48,13 +57,25 @@ export const Container = styled.div`
     }
 `
 
+export const Me_img = styled.div`
+    
+    border: 1px solid rgba(224, 224, 224, 0.3);
+    border-radius: 8px;
+    overflow: hidden;
+    height: 420px;
+    animation-duration: 2s;
+    img{
+        height: 430px;
+        padding-top: 20px;
+    }
+    
+`
+
 export const Itens = styled.ul`
     list-style-type: none;
     padding: 0;
     li{
         display: flex;
-        flex-direction: row;
-        align-items: center;
         gap: 5px;
         img{
             width: 18px;
@@ -101,7 +122,7 @@ export const Buttons = styled.section`
     }
 
     #tt{
-        width: 27px;
+        width: 22px;
         opacity: 0;
         transition: opacity 0.3s ease-in-out;
     }
@@ -126,4 +147,13 @@ export const Buttons = styled.section`
         opacity: 0;
     }
 
+    button:hover #tt{
+        transition: opacity 0.3s ease-in-out;
+        opacity: 1;
+    }
+
+    button:hover #tt2{
+        transition: opacity 0.3s ease-in-out;
+        opacity: 0;
+    }
 `
